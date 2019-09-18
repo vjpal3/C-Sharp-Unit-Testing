3367cda281b7f80c33833b70692771b734d85952
vjpal3 <vrishalipal@yahoo.com>
2019-09-18 12:20:23 -0400

    Use Nunit Setup() and Teardown()

---
+++ CharacterTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+
+
+namespace BasicTDD.Tests
+{
+    [TestFixture]
+    class CharacterTests
+    {
+        private Character character;
+
+        [SetUp]
+        public void Setup()
+        {
+            character = new Character(Type.Elf);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            character.Dispose();
+            character = null;
+        }
+
+        [Test]
+        public void IsDead_KillCharacter_ReturnsTrue()
+        {
+            character.Damage(500);
+            Assert.That(character.IsDead, Is.True);
+        }
+
+        [Test]
+        public void IsDead_DefaultCharacter_ReturnsFalse()
+        {
+            Assert.That(character.IsDead, Is.False);
+        }
+    }
+}
