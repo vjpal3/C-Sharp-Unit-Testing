58fca1a4af8d406fb9692afb1855da2b77e9c3bc
vjpal3 <vrishalipal@yahoo.com>
2019-09-17 21:04:00 -0400

    Unit testing using NUnit Framework

---
+++ SerialPortParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicTDD
+{
+    public class SerialPortParser
+    {
+        public static int ParsePort(string port)
+        {
+            if (!port.StartsWith("COM"))
+                throw new FormatException("Port is not in correct format");
+            else
+            {
+                const int lastIndexOfPrefix = 3;
+                string portNumber = port.Substring(lastIndexOfPrefix);
+                return int.Parse(portNumber);
+            }
+        }
+    }
+}
---
+++ SerialPortParserTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BasicTDD.Tests
+{
+    [TestFixture]
+    public class SerialPortParserTests
+    {
+        [Test]
+        public void ParsePort_COM1_Reurns1()
+        {
+            int result = SerialPortParser.ParsePort("COM1");
+            Assert.That(result, Is.EqualTo(1));
+
+        }
+    }
+}
