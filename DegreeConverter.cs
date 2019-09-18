3d38b18da9ed720109952a0b659e015f9a7bb844
vjpal3 <vrishalipal@yahoo.com>
2019-09-17 22:50:13 -0400

    Write unit test for converting Fahrenheit to Celsius and vice versa

---
+++ DegreeConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicTDD
+{
+    public class DegreeConverter
+    {
+        public double ToFahrenheit(double celsius)
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+
+        public double ToCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5 / 9;
+        }
+    }
+}
---
+++ DegreeConverterTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BasicTDD.Tests
+{
+    [TestFixture]
+    class DegreeConverterTests
+    {
+        [Test]
+        public void ToFahrenheit_ZeroCelsius_Returns32()
+        {
+            var result = new DegreeConverter().ToFahrenheit(0);
+            Assert.That(result, Is.EqualTo(32));
+        }
+
+        [Test]
+        public void ToCelsius_ThirtytwoFahrenheit_Returns0()
+        {
+            var result = new DegreeConverter().ToCelsius(32);
+            Assert.That(result, Is.EqualTo(0));
+        }
+    }
+}
