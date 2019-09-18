72d2c6929576055a91b246f81560a49bbe9a0d76
vjpal3 <vrishalipal@yahoo.com>
2019-09-18 16:55:54 -0400

    Write NUnit tests with TeseCases attribute for FizzBuzz

---
+++ FizzBuzzTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BasicTDD.Tests
+{
+    [TestFixture]
+    class FizzBuzzTests
+    {
+        [TestCase(3, "Fizz")]
+        [TestCase(5, "Buzz")]
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(17, "")]
+       public void Ask_Number_ReturnsCorrectString(int number, string expectedValue)
+        {
+            string result = FizzBuzz.Ask(number);
+            Assert.That(result, Is.EqualTo(expectedValue));
+
+        }
+
+    }
+}
