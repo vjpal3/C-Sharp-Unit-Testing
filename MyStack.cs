0f69eada6679543b96a2724d1832fd1d670c38c4
vjpal3 <vrishalipal@yahoo.com>
2019-09-19 14:36:14 -0400

    Test implementation of stack using NUnit

---
+++ MyStack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicTDD
+{
+    public class MyStack<T>
+    {
+        private readonly List<T> _list = new List<T>();
+        public bool IsEmpty => Count == 0;
+        public int Count => _list.Count;
+
+        public void Push(T value)
+        {
+            _list.Add(value);
+        }
+
+        public void Pop()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException();
+            _list.RemoveAt(Count - 1);
+        }
+
+        public T Peek()
+        {
+            return _list[Count - 1];
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
---
+++ MyStackTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace BasicTDD.Tests
+{
+    [TestFixture]
+    class MyStackTests
+    {
+        private MyStack<string> stack;
+        [SetUp]
+        public void Setup()
+        {
+            stack = new MyStack<string>();
+            ;
+        }
+
+        public void Teardown()
+        {
+            stack.Dispose();
+            stack = null;
+        }
+
+        [Test]
+        public void IsEmpty_EmptyStack_ReturnsTrue()
+        {
+            Assert.That(stack.IsEmpty, Is.EqualTo(true));
+        }
+
+        
+        [Test]
+        public void Count_PushTwoItems_ReturnsTwo()
+        {
+            stack.Push("Hello");
+            stack.Push("World");
+            Assert.That(stack.IsEmpty, Is.EqualTo(false));
+            Assert.That(stack.Count, Is.EqualTo(2));
+        }
+
+
+        [Test]
+        public void Pop_EmptyStack_ThrowsException()
+        {
+            Assert.Throws<InvalidOperationException>(() => stack.Pop());
+        }
+
+        [Test]
+        public void Push_Item_AddsItemAtTheEndOfStack()
+        {
+            stack.Push("Microsoft");
+            Assert.That(stack.Count, Is.EqualTo(1));
+            Assert.That(stack.Peek(), Is.EqualTo("microsoft").IgnoreCase);           
+        }
+
+        [Test]
+        public void Pop_Item_RemoveFromEndOfStack()
+        {
+            stack.Push("Hello");
+            stack.Push("World");
+            stack.Pop();
+            Assert.That(stack.Count, Is.EqualTo(1));
+            Assert.That(stack.Peek(), Is.EqualTo("hello").IgnoreCase);
+        }
+
+        [Test]
+        public void Peek_PushTwoItems_ReturnsItemAtTheEndOfStack()
+        {
+            stack.Push("Hello");
+            stack.Push("World");
+            Assert.That(stack.Peek, Is.EqualTo("world").IgnoreCase);
+        }
+    }
+}
