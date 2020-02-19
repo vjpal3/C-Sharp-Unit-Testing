43b8bb042f0871d78d05d7c3630f201aba5b892e
Vrishali Pal <vrishalipal@yahoo.com>
2020-02-18 22:32:04 -0500

    Add String Tests

---
+++ CharacterTestsAdditional.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BasicTDD.Tests
+{
+    [TestFixture]
+    class CharacterTestsAdditional
+    {
+        #region String Tests
+        [Test]
+        public void ShouldSetName()
+        {
+            const string expectedValue = "John";
+            Character c = new Character(Type.Elf, expectedValue);
+
+            Assert.That(c.Name, Is.EqualTo(expectedValue));
+            Assert.That(c.Name, Is.Not.Empty);
+            Assert.That(c.Name, Contains.Substring("ohn"));
+        }
+
+        [Test]
+        public void ShouldSetNameCaseInsensitive()
+        {
+            const string expectedUpperCase = "JOHN";
+            const string expectedLowerCase = "john";
+            Character c = new Character(Type.Elf, expectedUpperCase);
+
+            Assert.That(c.Name, Is.EqualTo(expectedLowerCase).IgnoreCase);
+        }
+
+        #endregion 
+
+    }
+}
